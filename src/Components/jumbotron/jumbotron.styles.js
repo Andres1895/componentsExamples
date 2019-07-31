3c1a6e3602c97209d25393cf38765ba7d41576a0
Maturano <Andres.Maturano@Honeywell.com>
2019-07-31 11:38:08 -0500

    Styles created with styled component for jumbotron

---
+++ src/Components/jumbotron/jumbotron.styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const JumbotronGeneral = styled.div`
+    display:flex;
+    flex-direction: row;
+    background-image: url(https://via.placeholder.com/1500x550);
+`;
+
+export const Title= styled.div`
+    font-weigth: bold;
+
+`;
+
+export const Link = styled.a`
+    color: white;
+`;
+
+export const Paragraph = styled.p`
+    color: white;
+`;
+
+export const JumbotronItem = styled.div`
+    background-color: rgba(76, 175, 80, 0.3);
+    margin: 5px;
+    max-width: 100%;
+    flex-direction: row;
+`;
+
+export const Width80 = styled.div`
+    width: 80%;
+`;
+export const Width20 = styled.div`
+    width: 20%;
+`;
