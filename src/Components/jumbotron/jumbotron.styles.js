d189d9065cb29ea4d593ce4b4af4807e584e94fd
Maturano <Andres.Maturano@Honeywell.com>
2019-08-02 14:14:06 -0500

    changes in styles

--- src/Components/jumbotron/jumbotron.styles.js
+++ src/Components/jumbotron/jumbotron.styles.js
@@ -5,12 +5,11 @@ export const JumbotronGeneral = styled.div`
     display:flex;
     flex-direction: row;
     background-image: url(https://via.placeholder.com/1500x350);
-    
 `;
 
 export const Title= styled.h1`
-    font-family: Tahoma;
     font-size: ${props => props.smallTitle ? "20px" : "40px"};
+    margin:0;
 `;
 
 export const LinkContainer = styled.div`
@@ -25,33 +24,34 @@ export const Link = styled.a`
 export const Paragraph = styled.p`
     color: white;
     text-align: justify;
+    margin: 0;
 `;
 
 export const JumboItem = styled.div`
     background-color: rgba(0, 148, 152, 0.7);
-    justify-content: space-between;
     flex-direction: row;
 `;
 
 export const Width80 = styled.div`
     width: 70%;
+    height: 100;
 `;
 export const Width20 = styled.div`
     width: 30%;
+    height: 100;
 `;
 export const Width50 = styled.div`
-
+    width: 50%
+    height: 100;
 `;
 
 export const JumbotronContainer = styled.div`
   color: white;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+
 `;
 export const SecondJumbotronContainer = styled.div`
   color: white;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+
 `;
