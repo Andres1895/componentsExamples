20c995ae96ae1153cdac6326e32602d030743c2c
Maturano <Andres.Maturano@Honeywell.com>
2019-08-15 13:49:07 -0500

    Sponsor connection to json

--- src/Components/Sponsors/sponsorItem.js
+++ src/Components/Sponsors/sponsorItem.js
@@ -2,10 +2,24 @@ import React from 'react';
 import {GeneralContainer, BodyContainer, LeftContainer, RightContainer,
 Text, TextContainer} from './sponsors.styles'
 import {textsponsor} from './textSponsor'
-import sponsors from './images.json'
+import $ from 'jquery'
 
 class Sponsor extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      sponsor: []
+    };
+  }
+  componentDidMount(){
+    var json = require('./images.json');
+    $.getJSON(json, function(data){
+      this.setState({sponsor: data})
+      console.log("Json ", json)
+    })
+  }
  render(){
+  console.log(this.state.sponsor)
   return(
    <GeneralContainer>
     <BodyContainer>
@@ -17,7 +31,7 @@ class Sponsor extends React.Component{
       </TextContainer>
      </LeftContainer>
      <RightContainer>
-      Hola
+      
      </RightContainer>
     </BodyContainer>
    </GeneralContainer>
